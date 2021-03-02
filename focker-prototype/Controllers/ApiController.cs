a0af3a37fb19af2250db8b62b7f35622ed1e70d0
vszymon <vszymonwrobel@gmail.com>
2021-03-02 23:16:10 +0100

    feat(API): base controller with mediator added

---
+++ focker-prototype/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace focker_prototype.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public abstract class ApiController : ControllerBase
+    {
+        private ISender _sender;
+        public ISender Sender => _sender ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+    }
+}
