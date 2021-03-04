b7b46d33948828945031d37e6cd42961d40a3db9
vszymon <vszymonwrobel@gmail.com>
2021-03-05 00:02:21 +0100

    feat: process info controller added

---
+++ focker-prototype/Controllers/ProcessInfoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace focker_prototype.Controllers
+{
+    public class ProcessInfoController : ApiController
+    {
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(string id)
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+        [HttpGet("id")]
+        public async Task<IActionResult> GetStandardOutput(string id)
+        {
+            return Ok();
+        }
+
+    }
+}
