6e3ef78b7c0023e8d3c0e46563705a71cff26a18
vszymon <vszymonwrobel@gmail.com>
2021-03-05 00:02:37 +0100

    feat: create method implemented

--- focker-prototype/Controllers/ProcessManagmentController.cs
+++ focker-prototype/Controllers/ProcessManagmentController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using focker_prototype.Commands;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -10,17 +11,18 @@ namespace focker_prototype.Controllers
     public class ProcessManagmentController : ApiController
     {
         [HttpPost]
-        public async Task<IActionResult> Create() 
-        {
-            return Ok();
-        }
+        public async Task<IActionResult> Create(CreateProcessCommand command) =>
+            Ok(await Sender.Send(command));
+        
 
         [HttpPost]
         public async Task<IActionResult> Kill() 
         {
-            return Ok();
+            return Ok(); 
         }
-     
+    
+
+
 
     }
 }
