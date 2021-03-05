4d249c455bdd9bcfcef1413b9d1ad99a9e0cf0dc
vszymon <vszymonwrobel@gmail.com>
2021-03-05 20:27:33 +0100

    feat: query implemented, handler created (not implemented yet)

---
+++ focker-prototype/Queries/GetStandardOutput.cs
@@ -0,0 +1,26 @@
+﻿using focker_prototype.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace focker_prototype.Queries
+{
+    public class GetStandardOutputQuery : IRequest<ProcessOutputViewModel>
+    {
+        public GetStandardOutputQuery(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+    public class GetStandardOutputQueryHandler : IRequestHandler<GetStandardOutputQuery, ProcessOutputViewModel>
+    {
+        public Task<ProcessOutputViewModel> Handle(GetStandardOutputQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
