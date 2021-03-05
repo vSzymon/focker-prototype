79da13c1255b8e58657e1f91ca14312ccc339f65
vszymon <vszymonwrobel@gmail.com>
2021-03-05 20:27:07 +0100

    feat: queries/handlers implemented

---
+++ focker-prototype/Queries/GetAllProcesses.cs
@@ -0,0 +1,21 @@
+﻿using focker_prototype.ViewModels;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace focker_prototype.Queries
+{
+    public class GetAllProcessesQuery : IRequest<IEnumerable<ProcessInfoViewModel>>
+    {
+    }
+    public class GetAllProcessesQueryHandler : IRequestHandler<GetAllProcessesQuery, IEnumerable<ProcessInfoViewModel>>
+    {
+        public Task<IEnumerable<ProcessInfoViewModel>> Handle(GetAllProcessesQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ focker-prototype/Queries/GetProcess.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace focker_prototype.Queries
+{
+    public class GetProcessQuery : IRequest<ProcessInfoViewModel>
+    {
+        public GetProcessQuery(int id)
+        {
+            Id = id;
+        }
+        public int Id { get; set; }
+    }
+    public class GetProcessQueryHandler : IRequestHandler<GetProcessQuery, ProcessInfoViewModel>
+    {
+        public Task<ProcessInfoViewModel> Handle(GetProcessQuery request, CancellationToken cancellationToken)
+        {
+            using var processInfo = Process.GetProcessById(request.Id);
+           
+            ProcessInfoViewModel processVM = new() 
+            {
+                Id = processInfo.Id,
+                Name = processInfo.ProcessName
+            };
+
+            return Task.FromResult(processVM);
+            
+        }
+    }
+}
