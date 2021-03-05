7152e4e50b27c287703d77f80fb9c7c51c21dcf0
vszymon <vszymonwrobel@gmail.com>
2021-03-05 20:28:08 +0100

    feat: view models added for queries return types

---
+++ focker-prototype/ViewModels/ProcessInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿
+namespace focker_prototype.ViewModels
+{
+    public record ProcessInfoViewModel
+    {
+        public int Id { get; init; }
+        public string Name { get; init; }
+    }
+}
---
+++ focker-prototype/ViewModels/ProcessOutputViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace focker_prototype.ViewModels
+{
+    public record ProcessOutputViewModel
+    {
+        public IEnumerable<string> OutputTextLines { get; init; }
+    }
+}
