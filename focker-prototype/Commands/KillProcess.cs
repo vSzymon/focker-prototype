226cd703e4715589563763d5abe75512344fedbe
vszymon <vszymonwrobel@gmail.com>
2021-03-05 20:25:29 +0100

    feat: kill process command/handler added

---
+++ focker-prototype/Commands/KillProcess.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace focker_prototype.Commands
+{
+    public class KillProcessCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+    public class KillProcessCommandHandler : IRequestHandler<KillProcessCommand>
+    {
+        public Task<Unit> Handle(KillProcessCommand request, CancellationToken cancellationToken)
+        {
+            using var processInfo = Process.GetProcessById(request.Id);
+            processInfo.Kill();
+            return Unit.Task;
+        }
+    }
+}
